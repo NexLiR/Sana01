f517b9a26eb774f73e82a6ac1ca062a05eee12d7
Vlad Shevcuk <ipz231_shvye@student.ztu.edu.ua>
2023-11-06 22:23:43 +0200

    Add variable input

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        double a, b, c, d, x, y, z, r;
+        Console.Write("Ender value for these variables:\na = ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        b = double.Parse(Console.ReadLine());
+        Console.Write("c = ");
+        c = double.Parse(Console.ReadLine());
+        Console.Write("d = ");
+        d = double.Parse(Console.ReadLine());
+    }
+}
